c16a1851cba907bc9e3f5073c76869ed42d48d75
Camille1905 <bourcamille00@gmail.com>
2021-12-20 17:31:27 +0100

    initialise C contraintes manualy

--- main.py
+++ main.py
@@ -3,5 +3,21 @@ import numpy as np
 # number of vertices
 n = 10
 
+def initialiseArrays(unaire, contrainte):
+    unaire[1][0] = True
+    unaire[1][2] = True
+    contrainte[1][2][2][0] = True
+    contrainte[1][3][0][2] = True
+    contrainte[1][3][1][1] = True
+    contrainte[1][5][2][0] = True
+    contrainte[1][5][1][2] = True
+    contrainte[1][5][2][1] = True
+    contrainte[3][4][1][1] = True
+    contrainte[3][5][2][0] = True
+
 if __name__ == '__main__':
+    unaire = np.full((n, 3), False)
     contrainte = np.full((n, n, 3, 3), False)
+
+    # Initialise C
+    initialiseArrays(unaire, contrainte)
