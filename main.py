66c9b5aa707d07ef325be7132a8f6c8b12345526
Camille1905 <bourcamille00@gmail.com>
2021-12-22 15:03:14 +0100

    3.1

--- main.py
+++ main.py
@@ -3,39 +3,80 @@ import numpy as np
 # number of vertices
 n = 10
 
-def initialiseArrays(unaire, contrainte):
-    unaire[1][0] = True
-    unaire[1][2] = True
-    unaire[1][1] = True
-    contrainte[1][2][2][0] = True
-    contrainte[1][3][0][2] = True
-    contrainte[1][3][1][1] = True
-    contrainte[1][5][2][0] = True
-    contrainte[1][5][1][2] = True
-    contrainte[1][5][2][1] = True
-    contrainte[3][4][1][1] = True
-    contrainte[3][5][2][0] = True
+
+def initialise_arrays(unary, constraints):
+    unary[1][0] = True
+    unary[1][2] = True
+    constraints[1][2][2][0] = True
+    constraints[1][3][0][2] = True
+    constraints[1][3][1][1] = True
+    constraints[1][5][2][0] = True
+    constraints[1][5][1][2] = True
+    constraints[1][5][2][1] = True
+    constraints[3][4][1][1] = True
+    constraints[3][5][2][0] = True
+
 
 if __name__ == '__main__':
-    unaire = np.full((n, 3), False)
-    contrainte = np.full((n, n, 3, 3), False)
+    unary = np.full((n, 3), False)
+    constraints = np.full((n, n, 3, 3), False)
 
     # Initialise C
-    initialiseArrays(unaire, contrainte)
+    initialise_arrays(unary, constraints)
 
-    nbBin = np.count_nonzero(contrainte == True)
+    nbBin = np.count_nonzero(constraints == True)
     print(nbBin)
 
-    nbUn = np.count_nonzero(unaire == True)
+    nbUn = np.count_nonzero(unary == True)
     print(nbUn)
 
     while (nbUn > 0) or (nbBin > 0):
         if nbUn >= 3:
-            for i in range (n):
-                if unaire[i][0] == True and unaire[i][1] == True and unaire[i][2] == True:
+            for i in range(n):
+                # case 1
+                if unary[i][0] is True and unary[i][1] is True and unary[i][2] is True:
                     print("Il n'y a pas de solution. Good bye!")
                     exit(0)
-                else:
-                    print("Pour l'instant c'est ok!")
-        else:
-            print("Pour l'instant c'est ok!")
+        for i in range(n):
+            # case 2
+            # delete all the constraints useless
+            if unary[i][0] is True and unary[i][1] is True:
+                for j in range(n):
+                    for k in range(2):
+                        print("k : ", k)
+                        constraints[i][j][0][k] = False
+                        constraints[i][j][1][k] = False
+                nbBin = np.count_nonzero(constraints is True)
+                break
+            elif unary[i][0] is True and unary[i][2] is True:
+                for j in range(n):
+                    for k in range(2):
+                        print("k : ", k)
+                        constraints[i][j][0][k] = False
+                        constraints[i][j][2][k] = False
+                nbBin = np.count_nonzero(constraints is True)
+                break
+            elif unary[i][1] is True and unary[i][2] is True:
+                for j in range(n):
+                    for k in range(2):
+                        print("k : ", k)
+                        constraints[i][j][1][k] = False
+                        constraints[i][j][2][k] = False
+                nbBin = np.count_nonzero(constraints is True)
+                break
+        # case 3
+        for i in range(n):
+            if unary[i][0]:
+                for j in range(n):
+                    for k in range(2):
+                        constraints[i][j][0][k] = False
+            elif unary[i][1]:
+                for j in range(n):
+                    for k in range(2):
+                        constraints[i][j][1][k] = False
+            elif unary[i][2]:
+                for j in range(n):
+                    for k in range(2):
+                        constraints[i][j][2][k] = False
+        nbBin = np.count_nonzero(constraints is True)
+        exit(0)
